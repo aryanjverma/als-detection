9014938fc926321c841f461e0a8207136b05bb55
aryanjverma <aryanjverma2@gmail.com>
2025-11-08 12:07:54 -0600

    trains model

--- test.py
+++ test.py
@@ -1,32 +1,67 @@
-from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import confusion_matrix
 import joblib
-X_test = []
-y_test = []
+X = []
+y = []
+
+# Function to generate a link given a number sample and whether they're normal or not
 def generate_link(letter, number):
     if number < 10:
-        return "ALSDetection_data/A0" + str(i) + ".csv"
+        return "ALSDetection_data/" + letter + "0" + str(number) + ".csv"
     else:
-        return "ALSDetection_data/A" + str(i) + ".csv"
+        return "ALSDetection_data/" + letter + str(number) + ".csv"
+# Turns a line in a csv to a vector of floats
 def line_to_vector(line):
     old_vector = line.split(',')
     new_vector = []
     for element in old_vector:
         new_vector.append(float(element))
     return new_vector
-for i in range(11,12):
+
+# Converts CSVs to matrices for training and testing  
+print("Processing data...")
+for i in range(1,12):
     a_link = generate_link("A", i)
     n_link = generate_link("N", i)
     with open(a_link, "r") as a:
         all_lines = a.readlines()
         for line in all_lines:
             vector = line_to_vector(line)
-            X_test.append(vector)
-            y_test.append(1)
+            X.append(vector)
+            y.append(1)
     with open(n_link, "r") as n:
         all_lines = n.readlines()
         for line in all_lines:
             vector = line_to_vector(line)
-            X_test.append(vector)
-            y_test.append(0)
-model = joblib.load("model.joblib")
-print(model.score(X_test,y_test))
+            X.append(vector)
+            y.append(0)
+print("Data processsed")
+model, scaler = joblib.load("model.joblib")
+# I used a MLP Classifier from Scikit Learn, using a leave-out training split where I left out one
+# nomral sample and one ALS sample for testing, as this was suggested by Dr.Wang to prevent over 
+# fitting. SVM was not good enough for my standards getting 70-80% accuracy, but this shallow neural
+# network got 94.6% accuracy.  
+
+X_test = scaler.transform(X)
+y_pred = model.predict(X_test) 
+conf_matrix = confusion_matrix(y, y_pred)
+print(conf_matrix)
+# Confusion Matrix:
+# 
+#                Predicted
+#               |  0   |   1  |
+#        -----------------------
+# Actual  0  | 1805 |  27 |    True Negatives (TN) = 1805, False Positives (FP) = 27
+#         1  |  111 | 624 |    False Negatives (FN) = 111, True Positives (TP) = 624
+#
+# Interpretation:
+# - The model correctly predicted 1805 negatives and 624 positives.
+# - It incorrectly predicted 27 positives that were actually negative (FP).
+# - It missed 111 positives that were predicted as negative (FN).
+sensitivity = conf_matrix[0][0] / (conf_matrix[0][0] + conf_matrix[0][1])
+specificity = conf_matrix[1][1] / (conf_matrix[1][1] + conf_matrix[1][0]) 
+print("Sensitivity: " + str(sensitivity))
+print("Specificity: " + str(specificity))
+print("Accuracy: " + str(model.score(X_test, y)))
+# Sensitivity: 0.9852620087336245
+# Specificity: 0.8489795918367347
+# Accuracy: 0.9462407479548111
--- train.py
+++ train.py
@@ -1,64 +1,79 @@
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
 import joblib
+import random
 
-X_train = []
+X_train_temp = []
 y_train = []
-X_test = []
+X_test_temp = []
 y_test = []
+# Function to generate a link given a number sample and whether they're normal or not
 def generate_link(letter, number):
     if number < 10:
         return "ALSDetection_data/" + letter + "0" + str(i) + ".csv"
     else:
         return "ALSDetection_data/" + letter + str(i) + ".csv"
+# Turns a line in a csv to a vector of floats 
 def line_to_vector(line):
     old_vector = line.split(',')
     new_vector = []
     for element in old_vector:
         new_vector.append(float(element))
     return new_vector
+# Picks random pair to use for testing
+randomNumber = random.randint(1,11)
+
+# Converts CSVs to matrices for training and testing  
 print("Processing data...")
-for i in range(1,11):
+for i in range(1,12):
     a_link = generate_link("A", i)
     n_link = generate_link("N", i)
-    with open(a_link, "r") as a:
-        all_lines = a.readlines()
-        for line in all_lines:
-            vector = line_to_vector(line)
-            X_train.append(vector)
-            y_train.append(1)
-    with open(n_link, "r") as n:
-        all_lines = n.readlines()
-        for line in all_lines:
-            vector = line_to_vector(line)
-            X_train.append(vector)
-            y_train.append(0)
-for i in range(11,12):
-    a_link = generate_link("A", i)
-    n_link = generate_link("N", i)
-    with open(a_link, "r") as a:
-        all_lines = a.readlines()
-        for line in all_lines:
-            vector = line_to_vector(line)
-            X_test.append(vector)
-            y_test.append(1)
-    with open(n_link, "r") as n:
-        all_lines = n.readlines()
-        for line in all_lines:
-            vector = line_to_vector(line)
-            X_test.append(vector)
-            y_test.append(0)
+    if (i != randomNumber):
+        with open(a_link, "r") as a:
+            all_lines = a.readlines()
+            for line in all_lines:
+                vector = line_to_vector(line)
+                X_train_temp.append(vector)
+                y_train.append(1)
+        with open(n_link, "r") as n:
+            all_lines = n.readlines()
+            for line in all_lines:
+                vector = line_to_vector(line)
+                X_train_temp.append(vector)
+                y_train.append(0)
+    else:
+        with open(a_link, "r") as a:
+            all_lines = a.readlines()
+            for line in all_lines:
+                vector = line_to_vector(line)
+                X_test_temp.append(vector)
+                y_test.append(1)
+        with open(n_link, "r") as n:
+            all_lines = n.readlines()
+            for line in all_lines:
+                vector = line_to_vector(line)
+                X_test_temp.append(vector)
+                y_test.append(0)
 print("Data processsed")
-print(str(len(X_train)) + " by " + str(len(X_train[0])) + " matrix")
+print(str(len(X_train_temp)) + " by " + str(len(X_train_temp[0])) + " matrix")
 print("Training model...")
+# Scales training data by standarizing data based on mean and variance
 scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.fit_transform(X_test)
-clf = MLPClassifier(random_state=3, max_iter=300, hidden_layer_sizes=(88,32), 
-                    learning_rate_init=0.00003).fit(X_train, y_train)
+X_train = scaler.fit_transform(X_train_temp)
+X_test = scaler.transform(X_test_temp)
+# The model is a shallow neural network. I used early stopping and alpha L2 regularization to 
+# prevent overfitting. 
+clf = MLPClassifier(random_state=42, max_iter=1500, hidden_layer_sizes=(64,32), 
+                    learning_rate_init=0.0005, early_stopping=True, 
+                    alpha=0.01).fit(X_train, y_train)
+
 print("Model trained.")
-print(clf.score(X_train, y_train))
-print(clf.score(X_test, y_test))
+print("Training score: " + str(clf.score(X_train, y_train)))
+print("Testing accuracy: " + str(clf.score(X_test, y_test)))
+
+# This saves the model. I commented it out because I already have a good model.
+'''
 filename = "model.joblib"
-joblib.dump(clf, filename)
+joblib.dump((clf, scaler), filename)
 print("Model saved.")
+'''
